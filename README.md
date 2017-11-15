e757cc345782d609d2c6fb575382fdcdedf6f87e
Sang Yeop Jung <sang.yeop.jung@noexternalmail.hsbc.com>
2017-11-15 17:40:59 +0800

    read dummy csv test

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 ## API
 
+#### (POST) http://localhost:3000/ISIN/
+* Get ISIN data from dummy bond data
+
 #### (GET) http://localhost:3000/entitySearch/{perm-id}
 * PermID Lookup
 * Example perm ID: 1-4295861160
--- routes/ISIN.js
+++ routes/ISIN.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var request = require('request');
+var csv = require('csvtojson');
 var router = express.Router();
+
+var csvFilePath = '../data/dummyBondData.csv';
 
 function getISIN(ISIN) {
     // TODO Read from excel
+    csv.fromFile(csvFilePath)
+        .on('json', function(jsonObj) {
+        console.log(jsonObj);
+    }).on('done', function(error) {
+        console.log('end');
+    });
+
     return "Details of ISIN " + ISIN + ": whatever";
 }
 
