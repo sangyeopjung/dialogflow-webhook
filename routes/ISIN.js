2453218e1058c795228d1700297362f6b0408de2
sangyeopjung <syjung812@gmail.com>
2017-11-17 13:59:57 +0800

    read csv (in development)

--- routes/ISIN.js
+++ routes/ISIN.js
@@ -1,28 +1,63 @@
-var express = require('express');
-var request = require('request');
-var csv = require('csvtojson');
-var router = express.Router();
+const express = require('express');
+const Converter = require('csvtojson').Converter;
+const router = express.Router();
 
-var csvFilePath = '../data/dummyBondData.csv';
+const csvFilePath = './data/dummyBondDataCsv.csv';
+let ISINList = [];
 
-function getISIN(ISIN) {
-    // TODO Read from excel
-    csv.fromFile(csvFilePath)
-        .on('json', function(jsonObj) {
+//TODO
+async function readCSV() {
+    var converter = new Converter({});
+
+    // await csv()
+    //     .fromFile(csvFilePath)
+    //     .on('json', function(jsonObj) {
+    //         list.push(jsonObj);
+    //     }).on('done', function(error) {
+    //         console.log('end');
+    //     });
+
+    await converter.fromFile(csvFilePath, function(err, result) {
+        if (err)
+            console.log(err);
+
+        let jsonObj = result;
         console.log(jsonObj);
-    }).on('done', function(error) {
-        console.log('end');
+        return jsonObj;
     });
 
-    return "Details of ISIN " + ISIN + ": whatever";
+    return list;
+}
+
+function getISIN(ISIN) {
+    for (let i = 0; i < ISINList.length; i++) {
+        console.log("comparing ", ISIN, ISINList[i]["ISIN"]);
+        if (ISIN === ISINList[i]["ISIN"]) {
+            return ISINList[i];
+        }
+    }
+    return null;
 }
 
 router.post('/', function(req, res, next) {
-    console.log(req);
-    var ISIN = req.body.result.parameters["ISIN"];
+    console.log(req.body);
+    const ISIN = req.body.result.parameters["ISIN"];
+    console.log(ISIN);
 
-    var output = getISIN(ISIN);
+    // if (ISINList.length === 0)
+    //     ISINList = readCSV();
+    //
+    // const jsonISIN = getISIN(ISIN);
+    // console.log(jsonISIN);
+    //
+    // let output;
+    // if (jsonISIN === null) {
+    //     output = "No data on ISIN " + ISIN + " found.";
+    // } else {
+    //     output = jsonObj.toString();
+    // }
 
+    let output = 'details on ' + ISIN + ' are .... whatever';
     res.status(200).send(JSON.stringify({'speech': output, 'displayText': output}));
 });
 
